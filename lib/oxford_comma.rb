00cd1fe4c79d2aff9f6955a668516b5f0cba738e
Moomal <moomalmsoomro@gmail.com>
2020-11-22 18:21:25 -0500

    commit tree 10c02491fc2b0aef079f08939ba0899cf22055f7

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+    if array.size <= 1
+        return array.join
+    elsif array.size == 2
+        return array.join(" and ")
+    end
 
+    new_string = array.shift.to_s
+    last_word = ", and " << array.pop.to_s
+
+    array.each do |word|
+        new_string << ", #{word}"
+    end
+    new_string << last_word
 end
